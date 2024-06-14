34dc8a16500833524a92ea99115c4b6fbc0c1730
김보람 <bory2321@naver.com>
2024-06-15 01:18:42 +0900

    db 5일차, 과제 끝

--- src/components/ButtonContainer.jsx
+++ src/components/ButtonContainer.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import './ButtonContainer.css';
+import "./ButtonContainer.css";
 
-const ButtonContainer = ({ createTodo }) => {
-    return (
-        <div className="button-container">
-            <button onClick={createTodo}>Todo 생성하기</button>
-        </div>
-    );
+const ButtonContainer = ({createTodo}) => {
+  return (
+    <div className="button-container">
+      <button onClick={createTodo}>Todo 생성하기</button>
+    </div>
+  );
 };
 
 export default ButtonContainer;
